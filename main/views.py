c49e166583f735264388fa316bf03d00364a1dbe
unknown <gibran.021105@gmail.com>
2025-09-14 17:43:26 +0700

    Migration to another laptop

--- main/views.py
+++ main/views.py
@@ -8,9 +8,9 @@ def show_main(request):
     news_list = News.objects.all()
 
     context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
-        'class': 'PBP A',
+        'npm' : '2406413842',
+        'name': 'Gibran Tegar Ramadhan Putra Lynardi',
+        'class': 'PBP D',
         'news_list': news_list
     }
 
